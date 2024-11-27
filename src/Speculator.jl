c8643aac5eb32c47cb72514e2edf49304a28b165
Jakob Peters <jakobj.peters@gmail.com>
2024-11-27 11:03:07 -0800

    Implement `ignore` and `log` keword parameters

--- src/Speculator.jl
+++ src/Speculator.jl
@@ -3,12 +3,15 @@ module Speculator
 
 export speculate!
 
-_speculate(modules, recursive, m, x::Module) =
-    if recursive && m != x push!(modules, x) end
+const cache = Set{Method}()
+
+_speculate(modules::Vector{Module}, recursive::Bool, _module::Module, x::Module) =
+    if recursive && _module != x push!(modules, x) end
 _speculate(_, _, _, @nospecialize x::Union{DataType, Function}) = for method in methods(x)
     sig = method.sig
 
-    if isconcretetype(sig)
+    if !(method in cache) && isconcretetype(sig)
+        push!(cache, method)
         parameter_types = getfield(sig, 3)[(begin + 1):end]
         precompile(x, ntuple(i -> parameter_types[i], length(parameter_types))) ||
             @warn "Precompilation failed, please file a bug report in Speculator.jl for:\n`$x($(
@@ -17,14 +20,26 @@ _speculate(_, _, _, @nospecialize x::Union{DataType, Function}) = for method in 
 end
 _speculate(_, _, _, @nospecialize _) = nothing
 
-function speculate!(modules::Vector{Module}; all = true, recursive = true)
+"""
+    speculate!(::Vector{Module};
+        all::Bool = true,
+        ignore::Vector{Symbol} = Symbol[],
+        log::Bool = true
+    )
+"""
+function speculate!(modules::Vector{Module};
+    all::Bool = true, ignore::Vector{Symbol} = Symbol[], log::Bool = true, recursive::Bool = true)
+    n, _ignore = length(cache), Set(ignore)
+
     while !isempty(modules)
         _module = pop!(modules)
 
         for name in names(_module; all)
-            _speculate(modules, recursive, _module, getfield(_module, name))
+            name in _ignore || _speculate(modules, recursive, _module, getfield(_module, name))
         end
     end
+
+    log && @info "Precompiled `$(length(cache) - n)` methods"
 end
 
 speculate!([Speculator])
