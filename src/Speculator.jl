0a0b887a09472ddf33b3b2c2c207504fdd10fe5a
Jakob Peters <jakobj.peters@gmail.com>
2024-11-27 10:57:41 -0800

    Use a function barrier

--- src/Speculator.jl
+++ src/Speculator.jl
@@ -3,26 +3,26 @@ module Speculator
 
 export speculate!
 
+_speculate(modules, recursive, m, x::Module) =
+    if recursive && m != x push!(modules, x) end
+_speculate(_, _, _, @nospecialize x::Union{DataType, Function}) = for method in methods(x)
+    sig = method.sig
+
+    if isconcretetype(sig)
+        parameter_types = getfield(sig, 3)[(begin + 1):end]
+        precompile(x, ntuple(i -> parameter_types[i], length(parameter_types))) ||
+            @warn "Precompilation failed, please file a bug report in Speculator.jl for:\n`$x($(
+        join(map(concrete_type -> "::" * string(concrete_type), parameter_types), ", ")))`"
+    end
+end
+_speculate(_, _, _, @nospecialize _) = nothing
+
 function speculate!(modules::Vector{Module}; all = true, recursive = true)
     while !isempty(modules)
         _module = pop!(modules)
 
         for name in names(_module; all)
-            x = getfield(_module, name)
-
-            if x isa Module recursive && x != _module && push!(modules, x)
-            elseif x isa Union{DataType, Function}
-                for method in methods(x)
-                    sig = method.sig
-
-                    if isconcretetype(sig)
-                        parameter_types = getfield(sig, 3)[(begin + 1):end]
-                        precompile(x, ntuple(i -> parameter_types[i], length(parameter_types))) ||
-                            @warn "Precompilation failed, please file a bug report in Speculator.jl for:\n`$x($(
-                        join(map(concrete_type -> "::" * string(concrete_type), parameter_types), ", ")))`"
-                    end
-                end
-            end
+            _speculate(modules, recursive, _module, getfield(_module, name))
         end
     end
 end
