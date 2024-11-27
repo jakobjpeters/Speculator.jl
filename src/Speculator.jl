c375bfedccbbe035b3d72bf7ab1c04bee5f7d565
Jakob Peters <jakobj.peters@gmail.com>
2024-11-27 10:31:53 -0800

    Simplify logic

--- src/Speculator.jl
+++ src/Speculator.jl
@@ -1,66 +1,32 @@
 
 module Speculator
 
-using Base: Iterators.product
+export speculate!
 
-export speculate
-
-const cache = Set()
-
-is_vararg(::typeof(Vararg)) = true
-is_vararg(_) = false
-
-function speculate(modules::Vector{Module}; all = true, recursive = true)
-    functions = []
-    types = []
-
+function speculate!(modules::Vector{Module}; all = true, recursive = true)
     while !isempty(modules)
         _module = pop!(modules)
-        _names = names(_module; all)
 
-        for name in _names
+        for name in names(_module; all)
             x = getfield(_module, name)
-            is_type = x isa DataType
 
-            recursive && x isa Module && x != _module && push!(modules, x)
-            (is_type || x isa Function) && push!(functions, x => methods(x))
-            if is_type
-                super_types = Set{Type}()
-                push!(types, x => super_types)
+            if x isa Module recursive && x != _module && push!(modules, x)
+            elseif x isa Union{DataType, Function}
+                for method in methods(x)
+                    sig = method.sig
 
-                while !(Any <: x)
-                    push!(super_types, x)
-                    x = supertype(x)
+                    if isconcretetype(sig)
+                        parameter_types = getfield(sig, 3)[(begin + 1):end]
+                        precompile(x, ntuple(i -> parameter_types[i], length(parameter_types))) ||
+                            @warn "Precompilation failed, please file a bug report in Speculator.jl for:\n`$x($(
+                        join(map(concrete_type -> "::" * string(concrete_type), parameter_types), ", ")))`"
+                    end
                 end
-            end
-        end
-    end
-
-    for ((type, super_types), (_function, sigs)) in product(types,
-        Iterators.map(((_function, methods),) -> _function => Iterators.filter(
-    isconcretetype, Iterators.map(method -> method.sig, methods)), functions))
-        in_super_types = in(super_types)
-
-        for sig in sigs
-            parameter_types = getfield(sig, 3)[(begin + 1):end]
-            concrete_types = ntuple(length(parameter_types)) do i
-                parameter_type = parameter_types[i]
-                in_super_types(parameter_type) ? type : parameter_type
-            end
-            signature = _function => concrete_types
-
-            if !in(signature, cache)
-                push!(cache, signature)
-                if !precompile(_function, concrete_types)
-                    s = join(map(concrete_type -> "::" * string(concrete_type), concrete_types), ", ")
-                    @warn "Precompilation failed, please file a bug report in Speculator.jl for:\n`$_function($s)`"
-                end
-                concrete_types
             end
         end
     end
 end
 
-speculate([Speculator])
+speculate!([Speculator])
 
 end # Speculator
