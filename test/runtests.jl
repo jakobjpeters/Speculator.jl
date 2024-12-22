86280b6a635181d9a877a6b385ba2e731fb7a68b
Jakob Peters <jakobj.peters@gmail.com>
2024-12-22 01:55:30 -0800

    Make tests runnable and passing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,44 +1,57 @@
 
-using MethodAnalysis: visit
-using PrecompileSignatures: precompilable
-using Speculator
-using Test: @test
+using MethodAnalysis, PrecompileSignatures, Speculator, Test
 
-function count_methods(predicate = Speculator.default_predicate; parameters...)
-    _read, _write = pipe = Pipe()
-    redirect_stderr(() -> speculate(Base; dry = true, verbosity = review, parameters...), pipe)
-    close(_write)
-    parse(Int, only(match(r"(\d+)", read(_read, String))))
+function count_methods(predicate, value; parameters...)
+    path = tempname()
+    speculate(predicate, value; path, parameters...)
+    length(readlines(path))
 end
+count_methods(value; parameters...) = count_methods(
+    Speculator.default_predicate,
+    value;
+parameters...)
 
-const _methods = Set{Method}()
-check_signature(x::DataType) = all(isconcretetype, x.types[(begin + 1):end])
-check_signature(_) = false
-function cache_methods(x::Method)
-    check_signature(x.sig) && x.module != Core && push!(_methods, x)
+speculator_count = count_methods(Base)
+precompile_signatures_count = length(
+    PrecompileSignatures.precompilables(Base, PrecompileSignatures.Config(; split_unions = false))
+)
+method_analysis_count = 0
+function count_method_analysis(x::Method)
+    sig = x.sig
+    if sig isa DataType && !(parentmodule(x) == Core && Tuple <: sig)
+        types = sig.types[2:end]
+        if (
+            (isempty(types) || !Base.isvarargtype(last(types))) &&
+            all(eachindex(types)) do i
+                isconcretetype(types[i]) || Speculator.is_subset(1, x.nospecialize >> (i - 1))
+            end
+        )
+            global method_analysis_count += 1
+        end
+    end
     true
 end
-cache_methods((@nospecialize _)) = true
-visit(cache_methods, Base)
-close(_write)
-count = count_methods()
-@test length(_methods) < count
-@test length(precompilables(Base)) < count
+count_method_analysis((@nospecialize _)) = true
+visit(count_method_analysis, Base)
+@test method_analysis_count < speculator_count
+@test precompile_signatures_count < speculator_count
 
 module X end
-const path = "precompile.jl"
+path = "precompile.jl"
 rm(path; force = true)
 speculate(X; path, dry = true)
 @test !isfile(path)
 speculate(X; path)
 @test isfile(path)
 
-@test_nowarn speculate()
+@test_nowarn speculate(Base; dry = true)
 
-const path = tempname()
+path = tempname()
 speculate(Base; path)
-@test (include(path); true)
+@test_broken (include(path); true)
+# include(x -> :(@test $x), path)
 
-@test issorted(map(maximum_methods -> count_methods(; maximum_methods), 1:10))
+@test issorted(map(limit -> count_methods(Base; limit), 1:4))
 
 @test count_methods(Returns(false)) == 0
+@test count_methods(Returns(false), () -> nothing) == 1
