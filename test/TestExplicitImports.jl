987a94e1b0dd535a788a56e254771578f5cf525a
Jakob Peters <jakobj.peters@gmail.com>
2025-06-28 01:55:02 -0700

    Fix ExplicitImports.jl test

--- test/TestExplicitImports.jl
+++ test/TestExplicitImports.jl
@@ -11,16 +11,16 @@ using Test: @test
 for (check, ignore) in [
     check_all_qualified_accesses_are_public => (:active_repl_backend, :active_repl, :map),
     check_all_explicit_imports_are_public => (
-        :Builtin, :IdSet, :Stateful, :TypeofBottom,
-        :Typeof, :isdeprecated, :issingletontype,
-        :isvarargtype, :loaded_modules_array, :mul_with_overflow, :specializations, :tail,
-        :typename, :uniontypes, :unsorted_names, :unwrap_unionall
+        :Builtin, :IdSet, :Stateful, :TypeofBottom, :Typeof,
+        :checked_mul, :isdeprecated, :issingletontype, :isvarargtype,
+        :loaded_modules_array, :mul_with_overflow, :specializations,
+        :tail, :typename, :uniontypes, :unsorted_names, :unwrap_unionall
     ),
     check_all_explicit_imports_via_owners => (),
     check_all_qualified_accesses_via_owners => (),
     check_no_implicit_imports => (),
     check_no_self_qualified_accesses => (:compile,),
-    check_no_stale_explicit_imports => (),
+    check_no_stale_explicit_imports => (:checked_mul,),
 ]
     @test isnothing(check(Speculator; ignore))
 end
